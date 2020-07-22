a037a5b0976b4db48c7278ef8481fa1de81e63f6
Sia Karamalegos <siakaramalegos@gmail.com>
2020-07-22 11:46:00 -0500

    Add ratings from omdb api

--- functions/movies.js
+++ functions/movies.js
@@ -1,8 +1,27 @@
+const { URL } = require('url')
+const fetch = require('node-fetch')
 const movies = require('../data/movies.json')
 
 exports.handler = async () => {
+  const api = new URL('https://omdbapi.com/')
+  api.searchParams.set('apikey', process.env.OMDB_API_KEY)
+
+  const promises = movies.map(movie => {
+    api.searchParams.set('i', movie.id)
+
+    return fetch(api)
+      .then(res => res.json())
+      .then(data => {
+        const scores = data.Ratings
+
+        return { ...movie, scores }
+      })
+  })
+
+  const moviesWithRatings = await Promise.all(promises)
+
   return {
     statusCode: 200,
-    body: JSON.stringify(movies)
+    body: JSON.stringify(moviesWithRatings)
   }
 }
